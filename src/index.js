b0672ccfb0fcd630927123727552dfae11d2dc2f
franco.balich@gmail.com <franco.balich@gmail.com>
2021-01-16 12:27:09 -0300

    Se realizo la estructura del proyecto

---
+++ src/index.js
@@ -0,0 +1,24 @@
+//Se auto completa el codigo de js por las extensiones de JavaScript (ES6) y Prettier
+const express = require("express");
+const app = express();
+const path = require("path");
+
+//settings
+app.set("port",3000);
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
+//settings
+
+//routes
+app.use(require("./routes/"));
+//routes
+
+//static files
+app.use(express.static(path.join(__dirname,"public")));
+//static files
+
+//port
+app.listen(app.get("port"),()=>{
+    console.log("Server run on Port: ", app.get("port"));
+});
+//port
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/",(req,res)=>{
+    res.render("index",{title:"Ejemplo de realidad aumentada"});
+});
+
+router.get("/contact",(req,res)=>{
+    res.render("contact",{title:"Ejemplo de realidad aumentada en la seccion contacto"});
+});
+module.exports=router;
